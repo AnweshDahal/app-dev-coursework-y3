a99499ec8d2eb48cf77b6fc2c25f5c9826605524
AnweshDahal <anweshd2000@gmail.com>
2022-01-01 10:50:00 +0530

    Adjust for changes

--- Coursework1/TicketRate.cs
+++ Coursework1/TicketRate.cs
@@ -8,14 +8,49 @@ namespace Coursework1
 {
     public class TicketRate
     {
+        /// <summary>
+        /// Base ticket rate for child
+        /// </summary>
         public float ChildBaseRate { get; set; }
+
+        /// <summary>
+        /// Base ticket rate for adult
+        /// </summary>
         public float AdultBaseRate { get; set; }
+
+        /// <summary>
+        /// Cost multiplier for a visit time of two hour
+        /// </summary>
         public float TwoHourMultiplier { get; set; }
+
+        /// <summary>
+        /// Cost multiplier for a visit time of three hour
+        /// </summary>
         public float ThreeHourMultiplier { get; set; }
+
+        /// <summary>
+        /// Cost multiplier for an unlimited visit time
+        /// </summary>
         public float UnlimitedMultiplier { get; set; }
+
+        /// <summary>
+        /// Discount for a group of five persons
+        /// </summary>
         public float GroupFiveMultiplier { get; set; }
+
+        /// <summary>
+        /// Discount for a group of ten persons
+        /// </summary>
         public float GroupTenMultiplier { get; set; }
+
+        /// <summary>
+        /// Discount for a visit in week day
+        /// </summary>
         public float WeekDayMultiplier { get; set; }
+
+        /// <summary>
+        /// Cost multiplier for a visit in weekends
+        /// </summary>
         public float WeekendMultiplier { get; set; }
     }
 }
